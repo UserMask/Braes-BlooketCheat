748fe20eaae38628ca5cc2688b0559275199a860
UserMask <104344459+UserMask@users.noreply.github.com>
2023-03-10 16:17:50 +1300

    Update gui.js

--- unobfuscated/gui.js
+++ unobfuscated/gui.js
@@ -116,7 +116,7 @@
                             fontWeight: "700",
                             userSelect: "text"
                         },
-                        innerHTML: "Ctrl + E to hide | Ctrl + X for quick disable<br>Click and drag here"
+                        innerHTML: "Ctrl + E to hide:) "
                     })),
                     createElement("div", {
                         id: "credits",
@@ -141,7 +141,7 @@
                             fontWeight: "700",
                             userSelect: "text"
                         },
-                        innerHTML: "Created by OneMinesraft2#5372"
+                        innerHTML: "Created by Brae"
                     }),
                     (controlButtons = createElement("div", {
                         id: "controlButtons",
@@ -272,7 +272,7 @@
                                 visibility: "visible"
                             },
                             target: "_blank",
-                            href: "https://discord.gg/QznzysxvX4",
+                            href: ":)",
                             innerHTML: `<div style="background: rgba(0,0,0,.25); border-radius: 5px; display: block; width: 100%; height: 100%; left: 0; top: 0; position: absolute; transform: translateY(2px); width: 100%; transition: transform .6s cubic-bezier(.3,.7,.4,1)"></div>
                 <div style="background-color: rgb(11, 194, 207); filter: brightness(.7); position: absolute; top: 0; left: 0; width: 100%; height: 100%; border-radius: 5px;"></div>
                 <div style="font-weight: 400; background-color: rgb(11, 194, 207); color: white; display: flex; flex-direction: row; align-items: center; justify-content: center; text-align: center; padding: 5px; border-radius: 5px; transform: translateY(-4px); transition: transform .6s cubic-bezier(.3,.7,.4,1)">
